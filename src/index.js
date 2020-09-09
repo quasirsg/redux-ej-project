4039307fef0910f1a5c90a39f8e08e24964fd7c4
Romano Javier <alexis_b13@live.com.ar>
2020-09-09 18:00:47 -0300

    Ejemplo terminado-tareas con redux y jquery

--- src/index.js
+++ src/index.js
@@ -1,1 +1,60 @@
-console.log("Hola mundo");
+import store from './redux/store';
+
+const itemListDom = $('#itemList');
+const itemDom = $('#item');
+const txtNuevaNotaDom = $('#txtNuevaNota');
+
+txtNuevaNotaDom.keyup((e)=>{
+    if(e.keyCode === 13){
+        const text = txtNuevaNotaDom.val();
+        txtNuevaNotaDom.val('');
+        store.dispatch({
+            type:'AGREGAR',
+            payload:{
+                text,
+            }
+        });
+    }
+});
+function actualizarLista(items) {
+    itemListDom.html('');
+    for (const item of items) {
+        const cloneDom = itemDom.clone();
+        const chkHabilitadoDom = cloneDom.find('input');
+        const btnBorrarDom = cloneDom.find('button');
+        const lblNombreDom = cloneDom.find('span');
+        cloneDom.removeClass('d-none');
+        
+        lblNombreDom.html(item.text); 
+        if (item.completado) {
+            lblNombreDom.css('text-decoration','line-through');
+        }
+        btnBorrarDom.on('click',()=>{
+            store.dispatch({
+                type: 'BORRAR',
+                payload: {
+                    id: item.id,
+                },
+            });
+        });
+        chkHabilitadoDom.prop('checked',item.completado);
+
+        chkHabilitadoDom.on('click',()=>{
+            store.dispatch({
+                type:'ALTERNAR',
+                payload: {
+                    id:item.id,
+                },
+            });
+        });
+
+        itemListDom.append(cloneDom);
+    }
+}
+
+store.subscribe(()=>{
+    const state = store.getState();
+
+    actualizarLista(state);
+});
+
